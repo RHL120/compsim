5b01bed1c9bc225082bb55732f8e32faedbc1aa7
rhl120 <rhl120@protonmail.com>
2022-10-16 14:51:15 +0200

    Implement the instruction set

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ struct Processor {
     instructions: [Instruction; 256], //The program being executed
 }
 
+#[derive(Clone, Copy)]
 enum Register {
     A,   //The first general purpose register
     B,   //The second general purpose register
@@ -14,6 +15,7 @@ enum Register {
     Rip, //the current instruction location
 }
 
+#[derive(Clone, Copy)]
 enum Instruction {
     Move(Register, u8),            //Set the given register to the given value
     Cpy(Register, Register),       //Copy the value of the second register to the first
@@ -23,13 +25,13 @@ enum Instruction {
     LoadToAddress(u8, Register),   //Loads the value in the given register to the given address
     JumpLessThan(u8), //jumps to the given instruction location if register a is less than register b
     JumpGreaterThan(u8), //jumps to the given instruction location if register a is greater than register b
-    JumpEqualTo(u8), //Jumps to the given instruction location if register a is equal to register b
+    JumpEqual(u8), //Jumps to the given instruction location if register a is equal to register b
+    Jump(u8),      //Jumps to the given instruction location
     Nop,
 }
 
 enum ExecutionError {
     IntegerOverflow,
-    OutOfBoundMem,
 }
 
 impl Processor {
@@ -43,7 +45,90 @@ impl Processor {
             instructions: program,
         }
     }
-    fn execute() -> Result<(), ExecutionError> {
+    fn get_register(&self, register: Register) -> u8 {
+        match register {
+            Register::A => self.register_a,
+            Register::B => self.register_b,
+            Register::Res => self.register_result,
+            Register::Rip => self.rip,
+        }
+    }
+    fn set_register(&mut self, register: Register, val: u8) {
+        let rf = match register {
+            Register::A => &mut self.register_a,
+            Register::B => &mut self.register_b,
+            Register::Res => &mut self.register_result,
+            Register::Rip => &mut self.rip,
+        };
+        *rf = val;
+    }
+    fn mov(&mut self, r: Register, v: u8) {
+        self.set_register(r, v);
+    }
+    fn cpy(&mut self, r1: Register, r2: Register) {
+        self.set_register(r1, self.get_register(r2));
+    }
+    fn sub(&mut self, r1: Register, r2: Register) -> Result<(), ExecutionError> {
+        let val1 = self.get_register(r1);
+        let val2 = self.get_register(r2);
+        if let Some(x) = val1.checked_sub(val2) {
+            self.register_result = x;
+            Ok(())
+        } else {
+            Err(ExecutionError::IntegerOverflow)
+        }
+    }
+    fn add(&mut self, r1: Register, r2: Register) -> Result<(), ExecutionError> {
+        let val1 = self.get_register(r1);
+        let val2 = self.get_register(r2);
+        if let Some(x) = val1.checked_add(val2) {
+            self.register_result = x;
+            Ok(())
+        } else {
+            Err(ExecutionError::IntegerOverflow)
+        }
+    }
+    fn load_from_addr(&mut self, r: Register, addr: u8) {
+        self.set_register(r, self.stack[addr as usize])
+    }
+    fn load_to_addr(&mut self, addr: u8, r: Register) {
+        self.stack[addr as usize] = self.get_register(r);
+    }
+    fn jump_less_than(&mut self, addr: u8) {
+        if self.register_a < self.register_b {
+            self.rip = addr;
+        }
+    }
+    fn jump_more_than(&mut self, addr: u8) {
+        if self.register_a > self.register_b {
+            self.rip = addr;
+        }
+    }
+    fn jump_equal(&mut self, addr: u8) {
+        if self.register_a == self.register_b {
+            self.rip = addr;
+        }
+    }
+    fn jump(&mut self, addr: u8) {
+        self.rip = addr;
+    }
+    fn execute(&mut self) -> Result<(), ExecutionError> {
+        while self.rip < 255 {
+            let instruction = self.instructions[self.rip as usize];
+            match instruction {
+                Instruction::Nop => (),
+                Instruction::Jump(addr) => self.jump(addr),
+                Instruction::JumpEqual(addr) => self.jump_equal(addr),
+                Instruction::JumpGreaterThan(addr) => self.jump_more_than(addr),
+                Instruction::JumpLessThan(addr) => self.jump_less_than(addr),
+                Instruction::LoadToAddress(addr, r) => self.load_to_addr(addr, r),
+                Instruction::LoadFromAddress(r, addr) => self.load_from_addr(r, addr),
+                Instruction::Add(r1, r2) => self.add(r1, r2)?,
+                Instruction::Sub(r1, r2) => self.sub(r1, r2)?,
+                Instruction::Move(r, v) => self.mov(r, v),
+                Instruction::Cpy(r1, r2) => self.cpy(r1, r2),
+            }
+        }
         Ok(())
     }
 }
