0c343588a91e8e86eba3f6864f54623fcf85d1a2
rhl120 <rhl120@protonmail.com>
2022-10-16 21:10:13 +0200

    Finish the backend

--- src/lib.rs
+++ src/lib.rs
@@ -18,16 +18,18 @@ pub enum Register {
 
 #[derive(Clone, Copy, Debug)]
 pub enum Instruction {
-    Move(Register, u8),            //Set the given register to the given value
-    Cpy(Register, Register),       //Copy the value of the second register to the first
+    Move(Register, u8),                  //Set the given register to the given value
+    Cpy(Register, Register),             //Copy the value of the second register to the first
     Sub(Register, Register), // Subtracts the first register from the second and stores the result in the result register
     Add(Register, Register), // Add the 2 registers and stroes the result in the result register
-    LoadFromAddress(Register, u8), //Loads the value of the given stack adress into the given register
-    LoadToAddress(u8, Register),   //Loads the value in the given register to the given address
-    JumpLessThan(u8), //jumps to the given instruction location if register a is less than register b
+    LoadFromAddress(Register, Register), //Loads the value of the stack adress stored into the second register into the given register
+    LoadToAddress(Register, Register), //Loads the value of the seconds register into the stack adress stored in the first register
+    JumpLessThan(u8), //Jumps to the given instruction location if register a is less than register b
     JumpGreaterThan(u8), //jumps to the given instruction location if register a is greater than register b
     JumpEqual(u8), //Jumps to the given instruction location if register a is equal to register b
     Jump(u8),      //Jumps to the given instruction location
+    Dump(Register), //Shows the value of the given register
+    DumpMem(Register), //Shows the value of the address stored in the register
     Nop,
 }
 
@@ -90,11 +92,14 @@ impl Processor {
             Err(ExecutionError::IntegerOverflow)
         }
     }
-    fn load_from_addr(&mut self, r: Register, addr: u8) {
-        self.set_register(r, self.stack[addr as usize])
+    fn load_from_addr(&mut self, r: Register, addr: Register) {
+        self.set_register(r, self.stack[self.get_register(addr) as usize])
     }
-    fn load_to_addr(&mut self, addr: u8, r: Register) {
-        self.stack[addr as usize] = self.get_register(r);
+    fn load_to_addr(&mut self, addr: Register, r: Register) {
+        //println!("the address: {}", self.get_register(addr) as usize);
+        //println!("the value: {}", self.get_register(r));
+        self.stack[self.get_register(addr) as usize] = self.get_register(r);
+        //println!("{}", self.stack[self.get_register(addr) as usize]);
     }
     fn jump_less_than(&mut self, addr: u8) {
         if self.register_a < self.register_b {
@@ -114,6 +119,12 @@ impl Processor {
     fn jump(&mut self, addr: u8) {
         self.rip = addr;
     }
+    fn dump(&self, r: Register) {
+        println!("{}", self.get_register(r));
+    }
+    fn dump_mem(&self, r: Register) {
+        println!("{}", self.stack[self.get_register(r) as usize]);
+    }
     pub fn execute(&mut self) -> Result<(), ExecutionError> {
         while self.rip < 255 {
             let instruction = self.instructions[self.rip as usize];
@@ -130,6 +141,8 @@ impl Processor {
                 Instruction::Sub(r1, r2) => self.sub(r1, r2)?,
                 Instruction::Move(r, v) => self.mov(r, v),
                 Instruction::Cpy(r1, r2) => self.cpy(r1, r2),
+                Instruction::Dump(r) => self.dump(r),
+                Instruction::DumpMem(r) => self.dump_mem(r),
             }
         }
         Ok(())
@@ -200,7 +213,7 @@ fn parse_add(line: u8, s: Vec<&str>) -> Result<Instruction, ParserError> {
 fn parse_load_from_address(line: u8, s: Vec<&str>) -> Result<Instruction, ParserError> {
     if s.len() == 3 {
         let register1 = parse_register(line, 1, s[1])?;
-        let address = parse_number(line, 2, s[2])?;
+        let address = parse_register(line, 2, s[2])?;
         Ok(Instruction::LoadFromAddress(register1, address))
     } else {
         Err(ParserError::InstructionLengthError(3, s.len()))
@@ -209,7 +222,7 @@ fn parse_load_from_address(line: u8, s: Vec<&str>) -> Result<Instruction, Parser
 fn parse_load_to_address(line: u8, s: Vec<&str>) -> Result<Instruction, ParserError> {
     if s.len() == 3 {
         let register1 = parse_register(line, 1, s[1])?;
-        let address = parse_number(line, 2, s[2])?;
+        let address = parse_register(line, 2, s[2])?;
         Ok(Instruction::LoadToAddress(address, register1))
     } else {
         Err(ParserError::InstructionLengthError(3, s.len()))
@@ -249,14 +262,34 @@ fn parse_jump(line: u8, s: Vec<&str>) -> Result<Instruction, ParserError> {
         Err(ParserError::InstructionLengthError(3, s.len()))
     }
 }
+fn parse_dump(line: u8, s: Vec<&str>) -> Result<Instruction, ParserError> {
+    if s.len() == 2 {
+        Ok(Instruction::Dump(parse_register(line, 1, s[1])?))
+    } else {
+        Err(ParserError::InstructionLengthError(3, s.len()))
+    }
+}
+
+fn parse_dump_mem(line: u8, s: Vec<&str>) -> Result<Instruction, ParserError> {
+    if s.len() == 2 {
+        Ok(Instruction::DumpMem(parse_register(line, 1, s[1])?))
+    } else {
+        Err(ParserError::InstructionLengthError(3, s.len()))
+    }
+}
+
 pub fn parse_program<'a>(string: &'a str) -> Result<[Instruction; 256], ParserError<'a>> {
     let mut program = [Instruction::Nop; 256];
-    let lines: Vec<&str> = string.split("\n").collect();
+    let lines: Vec<&str> = string.trim().split("\n").collect();
     if lines.len() > 256 {
         return Err(ParserError::LengthError(lines.len()));
     }
     for (i, line) in lines.iter().enumerate() {
-        let cols: Vec<&str> = line.trim().split(" ").collect();
+        let line = line.trim();
+        if line.starts_with("#") {
+            continue; //this is a comment
+        }
+        let cols: Vec<&str> = line.split(" ").collect();
         if cols.len() == 0 {
             continue;
         }
@@ -272,6 +305,8 @@ pub fn parse_program<'a>(string: &'a str) -> Result<[Instruction; 256], ParserEr
             "jgt" => parse_jump_greater_than(i as u8, cols)?,
             "jeq" => parse_jump_equal_to(i as u8, cols)?,
             "jmp" => parse_jump(i as u8, cols)?,
+            "dump" => parse_dump(i as u8, cols)?,
+            "dm" => parse_dump_mem(i as u8, cols)?,
             x => return Err(ParserError::UnknownKeyWord(i as u8, 0, x)),
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 mod lib;
 fn main() {
-    let program = lib::parse_program("mov a 2\nmov b 2\nadd a b").unwrap();
-    let mut proc = lib::Processor::new(program);
-    proc.execute().unwrap();
-    println!("{:#?}", proc);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprint!("Usage: {} <file path>", args[0]);
+        return;
+    }
+    let file = match std::fs::read_to_string(&args[1]) {
+        Err(e) => {
+            eprint!("Failed to open the file {}, error: {}", args[1], e);
+            return;
+        }
+        Ok(x) => x,
+    };
+    let script = match lib::parse_program(&file) {
+        Err(e) => {
+            eprint!("Failed to parse the file {}, error: {:#?}", args[1], e);
+            return;
+        }
+        Ok(x) => x,
+    };
+    let mut processor = lib::Processor::new(script);
+    match processor.execute() {
+        Err(e) => {
+            println!("Failed to execute the script: error: {:#?}", e);
+            return;
+        }
+        _ => (),
+    };
+    println!("{:#?}", processor.stack);
 }
